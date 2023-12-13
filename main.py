aa56353395823edceb03cb8131533a85397fe71e
Michael Feher <michael.feher@algorand.foundation>
2023-12-13 14:10:16 -0600

    feat: add close_outs and opt-ins

--- main.py
+++ main.py
@@ -22,6 +22,15 @@ def get_account_age(address):
     account = get(f"https://mainnet-idx.algonode.cloud/v2/accounts/{address}?include-all=true")
     return account["account"]["created-at-round"]
 
+def get_opt_ins(address):
+    data = get(f"https://mainnet-idx.algonode.cloud/v2/accounts/{address}")
+    if "account" in data:
+        acc = data["account"]
+        opt_in_apps = acc["total-apps-opted-in"]
+        opt_in_assets = acc["total-assets-opted-in"]
+        return opt_in_apps, opt_in_assets, False
+    else:
+        return 0, 0, True
 def get_transactions_before_vote(address, vote_block):
     tx = get(f"https://mainnet-idx.algonode.cloud/v2/transactions?address={address}&address-role=receiver&max-round={vote_block}")
     transactions = tx["transactions"]
@@ -42,8 +51,12 @@ def get_bab_transactions(votes, ages, next = None):
                 bytes = base64.b64decode(choice)[-5:]
                 choices = [int(bt) for bt in bytes]
                 if address not in votes["total"]:
+                    apps, assets, close_out = get_opt_ins(address)
                     txs_before_vote, first_transaction_from = get_transactions_before_vote(address, vote_block)
                     ages[address] = {
+                        "apps": apps,
+                        "assets": assets,
+                        "close_out": close_out,
                         "first_transaction_from": first_transaction_from,
                         "created_at_timestamp": get_block_timestamp(get_account_age(address)),
                         "received_transactions_before_vote": txs_before_vote,
@@ -67,7 +80,7 @@ try:
         ages = data["accounts"]
 except Exception as e:
     print(e)
-    
+
 
 next_token = get_bab_transactions(votes, ages)
 while next_token is not None:
